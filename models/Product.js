7b0ca5ff53b7382ba7ef57a1fd59aca4295074d0
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-11-27 03:31:49 -0500

    fixed the bug for put and update

--- models/Product.js
+++ models/Product.js
@@ -8,47 +8,52 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define product_name column
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define price column
+        price: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        // define stock column
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true
+            }
+        },
+        // define category_id column
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "category",
+                key: "id"
+            }
+        }
     },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      Validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id'
-      }
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
     }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
 );
 
 module.exports = Product;
